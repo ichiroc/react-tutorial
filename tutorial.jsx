bcdd4cca8ed5d4a60fa9ee3be3af9ab4cd6f8582
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-07-26 07:40:08 +0900

    Add rawMarkup.

--- tutorial.jsx
+++ tutorial.jsx
@@ -9,14 +9,18 @@ var CommentBox = React.createClass({
     }
 });
 var Comment = React.createClass({
-    render: function(){
+    rawMarkup: function(){
         var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return {__html: rawMarkup};
+    },
+    render: function(){
         return(
             <div className="comment">
               <h2 className="commentAuthor">
                 {this.props.author}
               </h2>
-              {md.render(this.props.children.toString())}
+              <span dangerouslySetInnerHTML={this.rawMarkup()}/>
             </div>
         );
     }
