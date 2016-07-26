968953e593bdac9c486499209208e019a1084140
中谷 一郎 <23054@skk.local>
2016-07-26 11:38:00 +0900

    8: Add data array which include comments.

--- tutorial.jsx
+++ tutorial.jsx
@@ -41,3 +41,9 @@ ReactDOM.render(
     <CommentBox />,
     document.getElementById("content")
 );
+
+
+var data = [
+    {id: 1, author: "Pete Hunt", text: "This is one comment"},
+    {id: 2, author: "Jordan Walke", text: "This is *another* comment"},
+];
