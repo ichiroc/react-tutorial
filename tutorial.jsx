8ac42503c71256e73532c17e16deb0ef60e9dfbb
中谷 一郎 <23054@skk.local>
2016-07-26 12:15:03 +0900

    13: Read comments json from url.

--- tutorial.jsx
+++ tutorial.jsx
@@ -1,15 +1,25 @@
-var data = [
-    {id: 1, author: "Pete Hunt", text: "This is one comment"},
-    {id: 2, author: "Jordan Walke", text: "This is *another* comment"},
-    {id: 3, author: "Ichiro", text: "This is **yet another** comment"},
-];
-
 var CommentBox = React.createClass({
+    getInitialState: function(){
+        return {data: []};
+    },
+    componentDidMount: function(){
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data){
+                this.setState({data:data});
+            }.bind(this),
+            error: function(xhr, status, err){
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
     render: function(){
         return(
                 <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data} />
+                <CommentList data={this.state.data} />
                 </div>
         );
     }
@@ -51,6 +61,6 @@ var Comment = React.createClass({
 });
 
 ReactDOM.render(
-        <CommentBox data={data} />,
+        <CommentBox url="api/comments" />,
     document.getElementById("content")
 );
