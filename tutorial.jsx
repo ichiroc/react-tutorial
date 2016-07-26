1ee34174f3f84cc4b24745576a6a25d171957256
中谷 一郎 <23054@skk.local>
2016-07-26 12:49:14 +0900

    Post したら、 author と text をクリアする。

--- tutorial.jsx
+++ tutorial.jsx
@@ -23,6 +23,7 @@ var CommentBox = React.createClass({
         return(
                 <div className="commentBox">
                 <h1>Comments</h1>
+                <CommentForm />
                 <CommentList data={this.state.data} />
                 </div>
         );
@@ -60,6 +61,36 @@ var Comment = React.createClass({
             </h2>
                 <span dangerouslySetInnerHTML={this.rawMarkup()}/>
                 </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    getInitialState : function(){
+        return {author: '', text: ''};
+    },
+    handleAuthorChange: function(e){
+        this.setState({author: e.target.value});
+    },
+    handleTextChange: function(e){
+        this.setState({text: e.target.value});
+    },
+    handleSubmit: function(e){
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if(!text || !author){
+            return;
+        }
+        this.setState({author: '', text: ''});
+    },
+    render: function(){
+        return(
+                <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} />
+                <input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange} />
+                <input type="submit" value="Post" />
+                </form>
         );
     }
 });
